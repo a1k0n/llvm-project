a632ca3c6c46c94f39875219576c56d61ef35280
Andy Sloane <andy@a1k0n.net>
2020-09-23 10:19:22 -0700

    add some more instruction formats (LJMP, @Ri)

--- llvm/test/MC/MCS51/mcs51-insns.s
+++ llvm/test/MC/MCS51/mcs51-insns.s
@@ -1,21 +1,26 @@
 # RUN: llvm-mc -triple mcs51 -show-encoding %s | FileCheck %s
 
-ADD  A, R0
-ADDC A, R1
-ADD  A, R2
-XRL  A, R7
-MOV  R2, A
-MOV  A, R3
-NOP
-RET
-
 # CHECK: .text
-# CHECK: ADD     A, R0                           # encoding: [0x28]
-# CHECK: ADDC    A, R1                           # encoding: [0x39]
-# CHECK: ADD     A, R2                           # encoding: [0x2a]
-# CHECK: XRL     A, R7                           # encoding: [0x6f]
-# CHECK: MOV     R2, A                           # encoding: [0xfa]
-# CHECK: MOV     A, R3                           # encoding: [0xeb]
-# CHECK: NOP                                     # encoding: [0x00]
-# CHECK: RET                                     # encoding: [0x22]
+CLR  A
+# CHECK: CLR  A     # encoding: [0xe4]
+ADD  A, R0
+# CHECK: ADD  A, R0 # encoding: [0x28]
+ADDC A, R1
+# CHECK: ADDC A, R1 # encoding: [0x39]
+ADD  A, R2
+# CHECK: ADD  A, R2 # encoding: [0x2a]
+XRL  A, R7
+# CHECK: XRL  A, R7 # encoding: [0x6f]
+MOV  R2, A
+# CHECK: MOV  R2, A # encoding: [0xfa]
+MOV  A, R3
+# CHECK: MOV  A, R3 # encoding: [0xeb]
+INC  @R0
+# CHECK: INC  @R0   # encoding: [0x06]
+NOP
+# CHECK: NOP        # encoding: [0x00]
+LJMP 0x1234
+# CHECK: LJMP 4660  # encoding: [0x02,0x12,0x34]
+RET
+# CHECK: RET        # encoding: [0x22]
 
