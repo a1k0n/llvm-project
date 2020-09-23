448aede9d2ddb36d2030420f1bf59de09bb73b65
Andy Sloane <andy@a1k0n.net>
2020-09-22 23:05:52 -0700

    adding some more insns

--- llvm/test/MC/MCS51/mcs51-insns.s
+++ llvm/test/MC/MCS51/mcs51-insns.s
@@ -3,8 +3,19 @@
 ADD  A, R0
 ADDC A, R1
 ADD  A, R2
+XRL  A, R7
+MOV  R2, A
+MOV  A, R3
+NOP
+RET
 
 # CHECK: .text
 # CHECK: ADD     A, R0                           # encoding: [0x28]
 # CHECK: ADDC    A, R1                           # encoding: [0x39]
 # CHECK: ADD     A, R2                           # encoding: [0x2a]
+# CHECK: XRL     A, R7                           # encoding: [0x6f]
+# CHECK: MOV     R2, A                           # encoding: [0xfa]
+# CHECK: MOV     A, R3                           # encoding: [0xeb]
+# CHECK: NOP                                     # encoding: [0x00]
+# CHECK: RET                                     # encoding: [0x22]
+
