d27b19e200cc911e79fce3e8b41accf81276129e
Andy Sloane <andy@a1k0n.net>
2020-09-23 16:25:03 -0700

    significantly refactored instructions, added disassembler + test

---
+++ llvm/lib/Target/MCS51/Disassembler/MCS51Disassembler.cpp
@@ -0,0 +1,139 @@
+//===-- MCS51Disassembler.cpp - Disassembler for MCS51 --------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the MCS51Disassembler class.
+//
+//===----------------------------------------------------------------------===//
+
+#include "MCTargetDesc/MCS51MCTargetDesc.h"
+#include "TargetInfo/MCS51TargetInfo.h"
+#include "llvm/MC/MCContext.h"
+#include "llvm/MC/MCDisassembler/MCDisassembler.h"
+#include "llvm/MC/MCFixedLenDisassembler.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCRegisterInfo.h"
+#include "llvm/MC/MCSubtargetInfo.h"
+#include "llvm/Support/Endian.h"
+#include "llvm/Support/TargetRegistry.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "MCS51-disassembler"
+
+typedef MCDisassembler::DecodeStatus DecodeStatus;
+
+namespace {
+class MCS51Disassembler : public MCDisassembler {
+
+public:
+  MCS51Disassembler(const MCSubtargetInfo &STI, MCContext &Ctx)
+      : MCDisassembler(STI, Ctx) {}
+  virtual ~MCS51Disassembler() {}
+
+  DecodeStatus getInstruction(MCInst &Instr, uint64_t &Size,
+                              ArrayRef<uint8_t> Bytes, uint64_t Address,
+                              raw_ostream &CStream) const override;
+};
+} // end anonymous namespace
+
+static MCDisassembler *createMCS51Disassembler(const Target &T,
+                                               const MCSubtargetInfo &STI,
+                                               MCContext &Ctx) {
+  return new MCS51Disassembler(STI, Ctx);
+}
+
+extern "C" void LLVMInitializeMCS51Disassembler() {
+  // Register the disassembler for each target.
+  TargetRegistry::RegisterMCDisassembler(getTheMCS51Target(),
+                                         createMCS51Disassembler);
+}
+
+static const unsigned GPRnDecoderTable[] = {
+  MCS51::R0,  MCS51::R1,  MCS51::R2,  MCS51::R3,
+  MCS51::R4,  MCS51::R5,  MCS51::R6,  MCS51::R7,
+};
+
+static DecodeStatus DecodeGPRnRegisterClass(MCInst &Inst, uint64_t RegNo,
+                                            uint64_t    Address,
+                                            const void *Decoder) {
+  if (RegNo > sizeof(GPRnDecoderTable)) return MCDisassembler::Fail;
+
+  // We must define our own mapping from RegNo to register identifier.
+  // Accessing index RegNo in the register class will work in the case that
+  // registers were added in ascending order, but not in general.
+  unsigned Reg = GPRnDecoderTable[RegNo];
+  Inst.addOperand(MCOperand::createReg(Reg));
+  return MCDisassembler::Success;
+}
+
+static DecodeStatus DecodeGPRiRegisterClass(MCInst &Inst, uint64_t RegNo,
+                                            uint64_t    Address,
+                                            const void *Decoder) {
+  if (RegNo > 1) return MCDisassembler::Fail;
+  return DecodeGPRnRegisterClass(Inst, RegNo, Address, Decoder);
+}
+
+/*
+template <unsigned N>
+static DecodeStatus decodeUImmOperand(MCInst &Inst, uint64_t Imm,
+                                      int64_t Address, const void *Decoder) {
+  assert(isUInt<N>(Imm) && "Invalid immediate");
+  Inst.addOperand(MCOperand::createImm(Imm));
+  return MCDisassembler::Success;
+}
+
+template <unsigned N>
+static DecodeStatus decodeSImmOperand(MCInst &Inst, uint64_t Imm,
+                                      int64_t Address, const void *Decoder) {
+  assert(isUInt<N>(Imm) && "Invalid immediate");
+  // Sign-extend the number in the bottom N bits of Imm
+  Inst.addOperand(MCOperand::createImm(SignExtend64<N>(Imm)));
+  return MCDisassembler::Success;
+}
+*/
+
+#include "MCS51GenDisassemblerTables.inc"
+
+DecodeStatus MCS51Disassembler::getInstruction(MCInst &Instr, uint64_t &Size,
+                                               ArrayRef<uint8_t> Bytes,
+                                               uint64_t          Address,
+                                               raw_ostream &CStream) const {
+  Size = 1;
+  if (Bytes.size() < 1) {
+    Size = 0;
+    return MCDisassembler::Fail;
+  }
+
+  uint32_t Insn = Bytes[0];
+  DecodeStatus r = decodeInstruction(DecoderTable8, Instr, Insn, Address, this, STI);
+  if (r != MCDisassembler::Fail) {
+    return r;
+  }
+
+  if (Bytes.size() < 2) {
+    Size = 0;
+    return MCDisassembler::Fail;
+  }
+
+  Insn |= (Bytes[1] << 8);
+  Size = 2;
+  r = decodeInstruction(DecoderTable16, Instr, Insn, Address, this, STI);
+  if (r != MCDisassembler::Fail) {
+    return r;
+  }
+
+  if (Bytes.size() < 3) {
+    Size = 0;
+    return MCDisassembler::Fail;
+  }
+
+  Insn |= (Bytes[2] << 16);
+  Size = 3;
+  return decodeInstruction(DecoderTable24, Instr, Insn, Address, this, STI);
+}
--- llvm/test/MC/MCS51/mcs51-insns.s
+++ llvm/test/MC/MCS51/mcs51-insns.s
@@ -1,46 +1,68 @@
-# RUN: llvm-mc -triple mcs51 -show-encoding %s | FileCheck %s
+# RUN: llvm-mc -triple mcs51 -show-encoding %s | FileCheck -check-prefixes=CHECK,CHECK-INST %s
+# RUN: llvm-mc -triple mcs51 -filetype obj < %s | llvm-objdump --triple=mcs51 -d - | FileCheck -check-prefix=CHECK %s
 
 # CHECK: .text
 CLR  A
-# CHECK: CLR  A     ; encoding: [0xe4]
+# CHECK: CLR  A
+# CHECK-INST: ; encoding: [0xe4]
 ADD  A, #4
-# CHECK: ADD  A, #4 ; encoding: [0x24,0x04]
+# CHECK: ADD  A, #4
+# CHECK-INST: ; encoding: [0x24,0x04]
 ADD  A, R0
-# CHECK: ADD  A, R0 ; encoding: [0x28]
+# CHECK: ADD  A, R0
+# CHECK-INST: ; encoding: [0x28]
 ADDC A, R1
-# CHECK: ADDC A, R1 ; encoding: [0x39]
+# CHECK: ADDC A, R1
+# CHECK-INST: ; encoding: [0x39]
 ADD  A, R2
-# CHECK: ADD  A, R2 ; encoding: [0x2a]
+# CHECK: ADD  A, R2
+# CHECK-INST: ; encoding: [0x2a]
 XRL  A, R7
-# CHECK: XRL  A, R7 ; encoding: [0x6f]
+# CHECK: XRL  A, R7
+# CHECK-INST: ; encoding: [0x6f]
 MOV  R2, A
-# CHECK: MOV  R2, A ; encoding: [0xfa]
+# CHECK: MOV  R2, A
+# CHECK-INST: ; encoding: [0xfa]
 MOV  A, R3
-# CHECK: MOV  A, R3 ; encoding: [0xeb]
+# CHECK: MOV  A, R3
+# CHECK-INST: ; encoding: [0xeb]
 INC  R0
-# CHECK: INC  R0    ; encoding: [0x08]
+# CHECK: INC  R0
+# CHECK-INST: ; encoding: [0x08]
 INC  @R0
-# CHECK: INC  @R0   ; encoding: [0x06]
+# CHECK: INC  @R0
+# CHECK-INST: ; encoding: [0x06]
 NOP
-# CHECK: NOP        ; encoding: [0x00]
+# CHECK: NOP
+# CHECK-INST: ; encoding: [0x00]
 LJMP 0x1234
-# CHECK: LJMP 4660  ; encoding: [0x02,0x12,0x34]
+# CHECK: LJMP 4660
+# CHECK-INST: ; encoding: [0x02,0x12,0x34]
 RET
-# CHECK: RET        ; encoding: [0x22]
+# CHECK: RET
+# CHECK-INST: ; encoding: [0x22]
 
 MOV A, #0x12
-# CHECK: MOV A, #18  ; encoding: [0x74,0x12]
+# CHECK: MOV A, #18
+# CHECK-INST: ; encoding: [0x74,0x12]
 XRL A, #0x80
-# CHECK: XRL A, #128 ; encoding: [0x64,0x80]
+# CHECK: XRL A, #128
+# CHECK-INST: ; encoding: [0x64,0x80]
 ORL A, #1
-# CHECK: ORL A, #1   ; encoding: [0x44,0x01]
+# CHECK: ORL A, #1
+# CHECK-INST: ; encoding: [0x44,0x01]
 XCH A, R7
-# CHECK: XCH A, R7   ; encoding: [0xcf]
+# CHECK: XCH A, R7
+# CHECK-INST: ; encoding: [0xcf]
 INC 0x45
-# CHECK: INC 69      ; encoding: [0x05,0x45]
+# CHECK: INC 69
+# CHECK-INST: ; encoding: [0x05,0x45]
 DEC 0x80
-# CHECK: DEC 128     ; encoding: [0x15,0x80]
+# CHECK: DEC 128
+# CHECK-INST: ; encoding: [0x15,0x80]
 XRL A, 0x45
-# CHECK: XRL A, 69   ; encoding: [0x65,0x45]
+# CHECK: XRL A, 69
+# CHECK-INST: ; encoding: [0x65,0x45]
 XRL A, @R1
-# CHECK: XRL A, @R1  ; encoding: [0x67]
+# CHECK: XRL A, @R1
+# CHECK-INST: ; encoding: [0x67]
